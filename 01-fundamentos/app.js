3e7eb8d0f1dca86aca64e470cfea9cd1ba1e4da6
fabianvcpz <gallardofabianvcpz@gmail.com>
2020-07-13 14:31:37 -0300

    segunda parte del curso

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,1 +1,13 @@
 console.log("Hola a todos");
+
+var primerNombre = 'grover';
+
+console.log(primerNombre);
+var edad = 25;
+var sueldo = 26500;
+
+console.log(edad,sueldo );
+
+var tieneTrabajo = true;
+
+console.log(tieneTrabajo);
