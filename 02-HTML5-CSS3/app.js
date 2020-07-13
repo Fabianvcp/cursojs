90afb55059b6f118765afc73ef8f400e2f80314e
fabianvcpz <gallardofabianvcpz@gmail.com>
2020-07-13 16:50:11 -0300

    sentencias condicionales

---
+++ 02-HTML5-CSS3/app.js
@@ -0,0 +1,13 @@
+/******************************
+ * Sentencia if / else
+ */
+
+var nombre ="pablo";
+var estadoCivil = "soltero";
+
+if(estadoCivil === "casado"){
+    //si es verdadera la condición
+    console.log(nombre +  ' Esta Casado');
+}else{
+    console.log(nombre +' esta soltero');
+}
