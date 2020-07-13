fae8af1099680d4d3aadfed41a1ca8446491d950
fabianvcpz <gallardofabianvcpz@gmail.com>
2020-07-13 16:12:01 -0300

    fin de la primera parte

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hola a todos");
 
 var primerNombre = 'grover';
@@ -10,4 +11,29 @@ console.log(edad,sueldo );
 
 var tieneTrabajo = true;
 
-console.log(tieneTrabajo);
+console.log(tieneTrabajo);*/
+
+//declarar varias variablea la vez;
+
+var primerNombre, edad, sexo;
+
+primerNombre='Grover';
+edad = 34;
+sexo='M';
+
+console.log('El nombre es:' + primerNombre + 'y su edad es:' + edad);
+
+//operador typeof
+
+//typeof me dira que tipo de variable console.log(typeof edad);
+
+//operador de incremento y decremento
+//++ incremento
+//-- decremento
+
+++ miEdad= 23;
+console.log("mi edad el año que viene" + miEdad);
+
+miEdad2 =23;
+console.log("mi edad actua"+ miEdad ++)
+
